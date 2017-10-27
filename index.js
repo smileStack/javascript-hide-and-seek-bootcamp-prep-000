608e493da1bb3c703efc61136b06456a2f364b1c
Ashley Martinez <ashley.carrasquel@gmail.com>
2017-10-27 04:42:16 +0000

    Done.

--- index.js
+++ index.js
@@ -18,9 +18,23 @@ function deepestChild(){
   var sav = document.querySelectorAll('#grand-node div');
   return sav[(sav.length-1)];
 }
-function increaseRankBy(num){
-   var sav = document.querySelectorAll('#ranked-list li');
+function increaseRankBy1(num){
+   var sav = document.querySelectorAll('.ranked-list li');
+   console.log(sav);
+
    for(let i=0; i<sav.length; i++){
+     console.log(sav[i].innerHTML);
+     sav[i].innerHTML =  parseInt(sav[i].innerHTML) + num;
+   }
+   console.log(sav);
+   return sav;
+}
+function increaseRankBy(num){
+   var sav = document.querySelectorAll('.ranked-list');
+   console.log(sav);
+
+   for(let i=0; i<sav.length; i++){
+     console.log(sav[i].innerHTML);
      sav[i].innerHTML =  parseInt(sav[i].innerHTML) + num;
    }
    console.log(sav);
